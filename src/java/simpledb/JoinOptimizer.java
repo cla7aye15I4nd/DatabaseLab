0b533511e503eade3db4cb9fa2baec274bd1069e
dataisland <dataisland@outlook.com>
2021-05-08 22:01:36 +0800

    Pass some trivial tests

--- src/java/simpledb/JoinOptimizer.java
+++ src/java/simpledb/JoinOptimizer.java
@@ -112,7 +112,10 @@ public class JoinOptimizer {
             // HINT: You may need to use the variable "j" if you implemented
             // a join algorithm that's more complicated than a basic
             // nested-loops join.
-            return -1.0;
+            if (j.p.equals(Predicate.Op.EQUALS))
+                return cost1 + card1 * cost2 + card1 + card2;
+            else
+                return cost1 + card1 * cost2 + card1 * card2;
         }
     }
 
@@ -156,9 +159,15 @@ public class JoinOptimizer {
             String field2PureName, int card1, int card2, boolean t1pkey,
             boolean t2pkey, Map<String, TableStats> stats,
             Map<String, Integer> tableAliasToId) {
-        int card = 1;
-        // some code goes here
-        return card <= 0 ? 1 : card;
+        if (joinOp == Predicate.Op.EQUALS) {
+            if (t1pkey && t2pkey) 
+                return Math.min(card1, card2);
+            if (t1pkey) 
+                return card2;
+            return card1;
+        }
+        
+        return card1 * card2;        
     }
 
     /**
